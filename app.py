6a4f109e18b269577a226c74f7b120e6bfa03c11
unknown <chotchaev.123@gmail.com>
2021-12-23 21:47:49 +0300

    Finished login part

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from re import match
 import flask
+import flask_session
 import help
 import sqlite3
 import m_imdb
@@ -9,6 +10,11 @@ sqliteConnection = sqlite3.connect("anime.db")
 
 app = flask.Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+flask_session.Session(app)
 
 @app.route("/")
 @help.LoginRequired
@@ -23,7 +29,7 @@ def main():
         return "Hello, mate!"
         # reloads page
     # returns main web page if not argiments were passed
-    return "Hello, mate!"   
+    return flask.render_template("main.html")   
 
 
 # get anime list function
@@ -78,4 +84,94 @@ def registered():
         flask.redirect("/register?errmsg=Your names sucks pls change")
     # load info to db
     help.AddRowToUserTable(name = username, password = password, favourite_title = favouriteTitle)
-    return flask.render_template("main.html", watchedTitles = "")
+    flask.session["id"] = help.GetUserId(username)
+    return flask.redirect(f"/?user_id={help.GetUserId(username)}")
+
+@app.route("/login", methods = ["POST", "GET"])
+def Login():
+    errmsg = "No errors"
+    if flask.request.args.get("errmsg"):
+        errmsg = flask.request.args.get("errmsg")
+
+    return flask.render_template("login.html", errmsg = errmsg)
+
+@app.route("/loginned", methods = ["GET", "POST"])
+def Loginned():
+    # check if user didn't type neither username nor password
+    if not flask.request.form.get("username") and not flask.request.form.get("password"):
+        return flask.redirect("/login?errmsg=You didn't write any info!!!")
+    # check if user didn't type his username
+    if not flask.request.form.get("username"):
+        return flask.redirect("/login?errmsg=You didn't enter your username")
+    # check if user didn't type his password
+    if not flask.request.form.get("password"):
+        return flask.redirect("/password?errmsg=You didn't enter your password")
+    
+    username = flask.request.form.get("username")
+    password = flask.request.form.get("password")
+
+    # check if there is a registered user with exactly same password and username
+    if help.CheckExisting(username) and help.CheckPassword(username, password):
+        flask.session["id"] = help.GetUserId(username)
+        return flask.redirect("/")
+
+    return flask.render_template("login.html", errmsg = "You entered wrong password")
+
+
+@app.route("/getTitles", methods = ["POST", "GET"])
+def GetTitles():
+    if flask.request.form.get("mode") == "unwatched":
+        # !!! this is debug version of the function !!!
+        # the proper version of function looks this wat
+        # help.GetUnwatchedTitles(flask.session["user_id"])
+        return str(help.GetUnwatchedTitles(4))
+    # !!! this is debug version of the function !!!
+    # the proper version of function looks this way
+    # watchedTitles = help.GetWatchedTitles(flask.session["user_id"])
+    watchedTitles = help.GetWatchedTitles(4)
+    print(watchedTitles)
+    return json.dumps(watchedTitles)
+
+
+
+@app.route("/AddTitle", methods = ["POST", "GET"])
+def AddTitle():
+    
+    
+    # Title's layout
+    # [0] - id
+    # [1] - name
+    # [2] - picture's url
+    # [3] - tags
+
+
+    # check if there is title's name
+    if not flask.request.form.get("titleName"):
+        return flask.redirect("/main?errmsg=You didn't wrote titleName")
+    
+    titleInfo = help.GetTitle(flask.request.form.get("titleName"))
+    # check if there is a rating and description (available only if it's watched title)
+    if flask.request.form.get("mode") == "watched":
+        if not flask.request.form.get("rating"):
+            return flask.redirect("/main?errmsg=You didn't select rating")
+        if not flask.request.form.get("description"):
+            return flask.redirect("/main?errmsg=You didn't write description")
+        # adding this title to user's watched titles database
+        title = {
+            "id": titleInfo[0],
+            "name": titleInfo[1],
+            "rating": int(flask.request.form.get("rating")),
+            "description": flask.request.form.get("description")
+        }
+        help.AddToWatchedTitles(title)
+        return "Successfully added title to watched titles"
+    if flask.request.form.get("mode") == "unwatched":
+        title = {
+            "id": titleInfo[0],
+            "name": titleInfo[1],
+        }
+        # debug version
+        help.AddToUnwatchedTitles(title, 4)
+        # release version
+        # help.AddToUnwatchedTitles(title, session["user_id"])
+        return "Successfully added title to unwatched titles"
--- help.py
+++ help.py
@@ -4,6 +4,11 @@ import flask
 import sqlite3
 
 
+# Title's layout
+# [0] - id
+# [1] - picture's url
+# [2] - tags
+
 
 def LoginRequired(func):
     @wraps(func)
@@ -12,9 +17,76 @@ def LoginRequired(func):
         if flask.request.args.get("user_id"):
             return func(*args, **kwargs)
         return flask.render_template("register.html", errmsg="No errors")
+        # above is older version 
+        # return flask.render_template("login.html", errmsg="No errors")
+        # above is new version 
 
     return DecoratedFunction
 
+
+def GetWatchedTitles(userID):
+    sqlconnection = sqlite3.Connection("anime.db")
+    db = sqlconnection.cursor()
+    titles = db.execute(f'SELECT * FROM watched_titles WHERE owners_id = {userID}').fetchall()
+    sqlconnection.commit()
+    titlesDicts = []
+    for title in titles:
+        titleElement = db.execute(f'SELECT * FROM titles WHERE id = {title[1]}').fetchall()[0]
+        sqlconnection.commit()
+        titleDict = {
+            "titleName": titleElement[1],
+            "description": title[3],
+            "rating": title[2],
+            "img": titleElement[2],
+            "tags": titleElement[3]
+        }
+        titlesDicts.append(titleDict)
+
+
+    # function returns a list where every element represents next dictionary
+    # titleName
+    # description
+    # rating
+    # img
+    # tags
+
+    return titlesDicts
+
+def GetUnwatchedTitles(userID):
+    sqlconnection = sqlite3.Connection("anime.db")
+    db = sqlconnection.cursor()
+    titles = db.execute(f'SELECT * FROM unwatched_titles WHERE owners_id = {userID}').fetchall()
+    sqlconnection.commit()
+    titlesDicts = []
+    for title in titles:
+        titleElement = db.execute(f'SELECT * FROM titles WHERE id = {title[1]}').fetchall()[0]
+        sqlconnection.commit()
+        titleDict = {
+            "titleName": titleElement[1],
+            "img": titleElement[2],
+            "tags": titleElement[3]
+        }
+        titlesDicts.append(titleDict)
+
+
+    # function returns a list where every element represents next dictionary
+    # titleName
+    # img
+    # tags
+
+    return titlesDicts
+
+
+def GetTitle(titleName):
+    sqlconnection = sqlite3.Connection("anime.db")
+    db = sqlconnection.cursor()
+    title = db.execute(f'SELECT * FROM titles WHERE title_name LIKE \'{titleName}%\'').fetchall()
+    # check if it could find anything
+    if len(title) == 0:
+        print("Couldn't find anything")
+    sqlconnection.commit()
+    # we return only 0 becouse there is only one element
+    return title[0]
 
 def AddRowToTitlesTable(**kwargs):
     """
@@ -38,24 +110,58 @@ def AddRowToUserTable(**kwargs):
     db = sqlconnection.cursor()
     db.execute(f'INSERT INTO users(name, password, favourite_title) VALUES(\"{kwargs["name"]}\", \"{kwargs["password"]}\" , \"{kwargs["favourite_title"]}\")')
     sqlconnection.commit()
-    # for i in kwargs.values():
 
 def CheckExisting(name):
     sqlconnection = sqlite3.Connection("anime.db")
     db = sqlconnection.cursor()
     names = db.execute(f'SELECT name FROM users').fetchall()
 
+    sqlconnection.commit()
+
+    if len(names) == 0:
+        return False
+
     for iter in names:
         if name in iter[0]:
             return True
     
-    sqlconnection.commit()
     return False
 
-# AddRowToUserTable(name = "Olzhas", password = "altaireiloveu", favourite_title = "Boku no Piko")
 def GetTitlesLike(titleName):
     sqlconnection = sqlite3.Connection("anime.db")
     db = sqlconnection.cursor()
     names = db.execute(f'SELECT * FROM titles WHERE title_name LIKE \'{titleName}%\' ').fetchall() 
     sqlconnection.commit()
     return names
+
+def GetUserId(userName):
+    sqlconnection = sqlite3.Connection("anime.db")
+    db = sqlconnection.cursor()
+    names = db.execute(f'SELECT id FROM users WHERE name = \'{userName}\' ').fetchall() 
+    sqlconnection.commit()
+    return int(names[0][0])
+
+
+def AddToWatchedTitles(title):
+    sqlconnection = sqlite3.Connection("anime.db")
+    db = sqlconnection.cursor()
+    # !!!this is release function !!!
+    # db.execute(f'INSERT INTO watched_titles (owners_id, title_id, rating, description) VALUES({flask.session["id"]}, {title["id"]}, {title["rating"]}, {title["description"]}')
+    # !!! this is debug function !!!
+    db.execute(f'INSERT INTO watched_titles (owners_id, title_id, rating, description) VALUES(4, {title["id"]}, {title["rating"]}, \'{title["description"]}\')')
+    sqlconnection.commit()
+
+def AddToUnwatchedTitles(title, userId):
+    sqlconnection = sqlite3.Connection("anime.db")
+    db = sqlconnection.cursor()
+    db.execute(f'INSERT INTO unwatched_titles (owners_id, title_id) VALUES({userId}, {title["id"]})')
+    sqlconnection.commit()
+
+def CheckPassword(username, passwordToCheck):
+    sqlconnection = sqlite3.Connection("anime.db")
+    db = sqlconnection.cursor()
+    password = db.execute(f'SELECT password FROM users WHERE name = \'{username}\'').fetchall()[0][0]
+    if passwordToCheck == password:
+        return True
+    return False
+
